3072326e2087ddd2faea8065775ca4ce60856130
wily ramos <wily.ramos1@unmsm.edu.pe>
2024-10-05 12:13:52 -0500

    update pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import AppRoutes from "./AppRoutes"
-import FooterSection from "./components/FooterSection"
-import Navbar from "./components/Navbar"
-import About from "./pages/About"
-import Home from "./pages/Home"
-import Login from "./pages/Login"
-import { Routes, Route } from "react-router-dom"
-import { BrowserRouter as Router } from "react-router-dom"
+import './App.css'
+import Navbar from './components/Navbar'
+import HeroSection from './components/HeroSection'
+import FooterSection from './components/FooterSection'
+
+
 
 
 export default function App() {
   return (
-    <div className="App">
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<div>Not Found</div>} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-      <FooterSection />
-    </Router>
-    </div>
-  )
+    <>
+      <div className="">
+        <Navbar />
+        <div className="">
+        <HeroSection />
+        </div>
+        <FooterSection />
+      </div>
+    </>
+  );
 }
--- src/ErrorBoundary.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function ErrorBoundary() {
-  return (
-    <div>ErrorBoundary</div>
-  )
-}
