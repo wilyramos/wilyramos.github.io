0d7a0618fa5c70b22c45b68215a87fadddcd4309
wily ramos <wily.ramos1@unmsm.edu.pe>
2025-02-10 14:49:54 -0500

    add new project socialynks whit ts+express

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -2,8 +2,8 @@ import ProjectCard from './ProjectCard';
 
 const projects = [
   { title: 'Teamly', description: 'Administrador de proyectos', imageUrl: '/5.svg', link: 'https://teamlyprojects.vercel.app/' },
-  { title: 'VisitCañete', description: 'Página web de turismo de la provincia de Cañete', imageUrl: '/4.svg', link: 'https://wilyramos.github.io' },
-  { title: 'Calculator', description: 'Calculadora web con operaciones básicas', imageUrl: '/5.svg', link: 'https://carwashcalculator.vercel.app/' },
+  { title: 'SociaLynks', description: 'Crea tu propia pagina centralizada para tus redes sociales', imageUrl: '/SocialLynks.png', link: 'https://socialynks.vercel.app/' },
+  { title: 'Calculator', description: 'Calculadora web con operaciones básicas', imageUrl: '/4.svg', link: 'https://carwashcalculator.vercel.app/' }
 ];
 
 export default function Projects() {
