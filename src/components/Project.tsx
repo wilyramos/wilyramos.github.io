671513941371ad02447f6ba8ec53323f45d17812
wily ramos <wily.ramos1@unmsm.edu.pe>
2025-02-11 19:02:19 -0500

    fix deploy

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -3,7 +3,7 @@ import ProjectCard from './ProjectCard';
 const projects = [
     { title: 'Teamly', description: 'Administrador de tus proyectos', imageUrl: '/5.svg', link: 'https://teamlyprojects.vercel.app/' },
     { title: 'SociaLynks', description: 'Crea tu propia pagina centralizada para tus redes sociales', imageUrl: '/socialynks.svg', link: 'https://socialynks.vercel.app/' },
-    { title: 'Visit Calango', description: 'Aplicacion de turismo para el distrito de Calango', imageUrl: '/calango.png', link: 'https://visitacalango.vercel.app/' }
+    { title: 'VisitCalango', description: 'Aplicacion de turismo para el distrito de Calango', imageUrl: '/calango.png', link: 'https://visitacalango.vercel.app/' }
 ];
 
 export default function Projects() {
