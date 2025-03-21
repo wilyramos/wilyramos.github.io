9690ea2c4a9c49664853a83d949ce256f2c07c9d
wily ramos <wily.ramos1@unmsm.edu.pe>
2025-03-21 13:43:57 -0500

    update routes hasrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
-import './App.css'
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
-import Principal from './components/principal'
-import Layout from './layout/layout'
+import './App.css';
+import { Route, HashRouter as Router, Routes } from 'react-router-dom'; // 👈 Cambia BrowserRouter por HashRouter
+import Principal from './components/principal';
+import Layout from './layout/layout';
 import Projects from './components/Project';
-
 
 export default function App() {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path='/' element={<Layout />}>
-            <Route path='/' element={<Principal />} />
-            <Route path='*' element={<h1>Not Found</h1>} />
-            <Route path='/about' element={<h1>About</h1>} />
-            <Route path='/proyectos' element={<Projects />} />
-          </Route>
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Principal />} />
+          <Route path="*" element={<h1>Not Found</h1>} />
+          <Route path="/about" element={<h1>About</h1>} />
+          <Route path="/proyectos" element={<Projects />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
