62a851f1b9212c65f7414805e0b8499d94848297
wily ramos <wily.ramos1@unmsm.edu.pe>
2025-03-20 20:37:03 -0500

    fix deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
 import './App.css'
-import { Route, HashRouter as Router, Routes } from 'react-router-dom'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Principal from './components/principal'
 import Layout from './layout/layout'
 import Projects from './components/Project';
 
+
+
+
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index element={<Principal />} />
-          <Route path='/about' element={<h1>About</h1>} />
-          <Route path='/proyectos' element={<Projects />} />
-          <Route path='*' element={<h1>Not Found</h1>} />
-        </Route>
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route path='/' element={<Principal />} />
+            <Route path='*' element={<h1>Not Found</h1>} />
+            <Route path='/about' element={<h1>About</h1>} />
+            <Route path='/proyectos' element={<Projects />} />
+          </Route>
+
+        </Routes>
+
+
+      </Router>
+    </>
   );
 }
