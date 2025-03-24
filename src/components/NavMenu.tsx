6b5f4d29cab4917c6a32676490024060781c563b
wily ramos <wily.ramos1@unmsm.edu.pe>
2025-03-24 17:38:58 -0500

    update navmenu

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -23,7 +23,7 @@ export default function NavMenu() {
                 <Popover.Panel className="absolute left-1/2 z-10 mt-5 flex w-screen lg:max-w-min -translate-x-1/2 lg:-translate-x-48">
                     <div className="w-full lg:w-56 shrink rounded-xl bg-white p-4 text-sm font-semibold leading-6 text-gray-900 shadow-lg ring-1 ring-gray-900/5">
                         <Link
-                            to='/profile'
+                            to='/about'
                             className='block p-2 hover:text-purple-950'
                         >Sobre mi</Link>
                         <Link
